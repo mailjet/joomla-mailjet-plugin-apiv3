3c85d87f995ab9f5dedc60dff69c59d29cb362b5
Oleksandr Mykhailenko <mihaskep@gmail.com>
2024-10-06 14:44:51 +0300

    Another part of changes =

--- packages/com_mailjet/admin/views/campaigns/tmpl/default.php
+++ packages/com_mailjet/admin/views/campaigns/tmpl/default.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * @author Mailjet SAS
+ * @author     Mailjet SAS
  *
  * @copyright  Copyright (C) 2014 Mailjet SAS.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 defined('_JEXEC') or die('Restricted access');
 
-$lib_dir = __DIR__.'/../../../lib/';
+$lib_dir = __DIR__ . '/../../../lib/';
 
-require_once ($lib_dir.'lib/vendor/autoload.php');
-require_once ($lib_dir.'config.php');
-require_once ($lib_dir.'lib/Auth.php');
-require_once ($lib_dir.'lib/mailjet-api-helper.php');
+require_once($lib_dir . 'lib/vendor/autoload.php');
+require_once($lib_dir . 'config.php');
+require_once($lib_dir . 'lib/Auth.php');
+require_once($lib_dir . 'lib/mailjet-api-helper.php');
 
 try {
 
-    $auth = new Auth();
+    $auth          = new Auth();
     $mailjetIframe = Mailjet_Api_Helper::getMailjetIframe($auth->getApiKey(), $auth->getApiSecret());
     $mailjetIframe->setInitialPage(\MailjetIframe\MailjetIframe::PAGE_CAMPAIGNS);
     ?>
@@ -25,10 +25,11 @@ try {
         <?php echo $this->sidebar; ?>
     </div>
     <div id="j-main-container" class="span10" style="width:100%; height: 1300px;">
-    <?php echo $mailjetIframe->getHtml(); ?>
+        <?php echo $mailjetIframe->getHtml(); ?>
     </div>
     <?php
-} catch (\MailjetIframe\MailjetException $e) {
+}
+catch (\MailjetIframe\MailjetException $e) {
     ?>
     <div id="j-main-container" class="span10" style="width:100%; height: 1300px;">
         <b>Error</b>
